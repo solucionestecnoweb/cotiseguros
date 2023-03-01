3ebf882de4179a964d6315ead85b86c3ccfd1940
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2023-03-01 17:37:02 -0400

    modulo de david cartaya

---
+++ ext/custom_cotiseguros/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Custom Cotiseguros",
+    "version": "15.0",
+    "summary": "Custom Cotiseguros",
+    "author": "David Cartaya",
+    "license": "AGPL-3",
+    "depends": ["base", "stock", "product"],
+    "data": [
+        'security/ir.model.access.csv',
+        'views/res_partner.xml',
+
+
+
+
+    ],
+    "installable": True,
+    "auto_install": True,
+    "application": False,
+}
---
+++ ext/custom_cotiseguros/models/res_partner.py
@@ -0,0 +1,20 @@
+from email.policy import default
+from odoo import models, fields, api
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    policy_number = fields.Char('Nro de Poliza')
+    type_partner = fields.Selection([('renewal','Renovacion'), ('new', 'Nuevo')], string='Tipo')
+    emission_date = fields.Date(string='Fecha de Emisión')
+    due_date = fields.Date(string='Fecha de Vencimiento')
+    age = fields.Char('Edad')
+    gender = fields.Selection([('male', 'Masculino'), ('female', 'Femenino')],
+                              string='Genero')
+    rif_ci = fields.Char('RIF/CI')
+    security_partner = fields.Many2one('security.partner', string="Aseguradora")
+
+class SecurityPartner(models.Model):
+    _name = 'security.partner'
+    
+    name = fields.Char(string='Aseguradora')
