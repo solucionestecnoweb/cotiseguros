f906590136e7c9bc86a448ea6be88cde8a8fda99
unknown <marilynmillan>
2023-03-07 17:10:34 -0400

    n de poliza de seguro beneficiario 09

--- ext/custom_cotiseguros/models/res_partner.py
+++ ext/custom_cotiseguros/models/res_partner.py
@@ -13,7 +13,7 @@ class ResPartner(models.Model):
                               string='Genero')
     rif_ci = fields.Char('RIF/CI')
     security_partner = fields.Many2one('security.partner', string="Aseguradora")
-   # benefi_number_policy= fields.Char('Nro de Poliza')
+    benefi_number_policy= fields.Char('Nro de Poliza')
     #benefi_emision_date= fields.Date(string='Fecha de Emisión')
     #benefi_date_due = fields.Date(string='Fecha de Vencimiento')
 
